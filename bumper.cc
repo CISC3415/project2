1cee7303a4eabe8b13ac6816f387c05685addac7
BENJYI <bentaebox@gmail.com>
2019-10-15 07:21:05 -0700

    add comments

--- bumper.cc
+++ bumper.cc
@@ -29,39 +29,49 @@ int main(int argc, char *argv[])
     if (yaw < 0) yaw += M_PI * 2;
     if (yaw > M_PI * 2) yaw -= M_PI * 2;
     
+    // If robot is stalled, nudge itself out by randomly
+    // moving back and forth and turning counter-clockwise
+    // until it is no longer stuck.
     if (pp.GetStall()) {
-      std::cout << "STALLED" << std::endl;
-
-      int sign = rand() % 2;
+      int sign = rand() % 2;            // random back & forth
       if (sign == 0) speed = -0.5;
-      else speed = 0.5;
-      std::cout << sign << std::endl;
-      turnrate = 0.8;
+      else speed = 0.5;                 
+      turnrate = 0.8;                   // counter-clockwise turn
+    
+    // If robot has previously hit both bumpers, turn at least 90 degrees.
     } else if (doublebumped) {
-      std::cout << "dblbump" << std::endl;
-      double anglediff = yaw-bumpangle;
-      if (anglediff < 0) anglediff += M_PI * 2;
-      if (anglediff > M_PI * 2) anglediff -= M_PI * 2;
-      if (anglediff < M_PI / 2) {
-        speed = -0.05;
+      double anglediff = yaw-bumpangle; // Difference between current
+                                        // angle and start angle.
+
+      if (anglediff < 0)                // Angle never below 0 rads
+        anglediff += M_PI * 2;          // and
+      if (anglediff > M_PI * 2)         // never exceeds 2*PI rads.
+        anglediff -= M_PI * 2;
+
+      if (anglediff < M_PI / 2) {       // If robot has not turned 90 deg
+        speed = -0.05;                  // keep turning and moving back.
         turnrate = 0.8;
-      } else {
-        turnrate = 0.0;
+      } else {                          // If robot has turned at least
+        turnrate = 0.0;                 // 90 degrees, resume normal route. 
         speed = 2.0;
         doublebumped = false;
       }
+    
+    // If robot first hits both bumpers, set bool doublebumped
+    // which then initiates a 90 degree turn.
     } else if (bp[0] && bp[1]) {
-      // turn 90 degrees
-      doublebumped = true;
-      std::cout << "DOUBLE BUMP" << std::endl;
-      bumpangle = yaw;
-      speed = -0.1;
+      doublebumped = true;              // Sets doublebumped.
+      bumpangle = yaw;                  // Sets start angle of robot.
+      speed = -0.1;                     // Move back to prevent stall.
+
+    // If robot hits left bumper, turn clockwise while moving back.
     } else if (bp[0]) {
-      std::cout << "LEFT BUMP" << std::endl;
       speed = -0.5;
       turnrate = -0.8;
+
+    // If robot hits right bumper, turn counter-clockwise while moving back.
     } else if (bp[1]) {
-      std::cout << "RIGHT BUMP" << std::endl;
+      speed = -0.5;
       turnrate = 0.8;
     } else {
       speed = 2.0;
